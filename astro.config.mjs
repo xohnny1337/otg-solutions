703887bb2d692b3e9b5193c984c9f47ffe9e1396
Johnny <xohnny1337@gmail.com>
2025-06-20 11:56:13 +0200

    lol

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
+// @ts-check
 import { defineConfig } from "astro/config";
-import netlify from "@astrojs/netlify/functions";
+import tailwindcss from "@tailwindcss/vite";
 
+// https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: netlify(),
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
